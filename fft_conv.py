f6f91ff68f28d311a96b52c540f8d928565d2381
Joey Yu <joeyyu@rayark.com>
2021-03-29 18:22:48 +0800

    draft

--- fft_conv.py
+++ fft_conv.py
@@ -1,10 +1,16 @@
 from functools import partial
 from typing import Tuple, Union, Iterable
+import math
 
 import torch
 from torch import nn, Tensor
-from torch.fft import rfftn, irfftn
+from torch.fft import rfftn, irfftn, ifftn, fftn, fftshift, ifftshift, rfft, irfft, fft, ifft
 import torch.nn.functional as f
+from torch.nn.modules.utils import _reverse_repeat_tuple
+
+
+def lcm(a, b):
+    return abs(a*b) // math.gcd(a, b)
 
 
 def complex_matmul(a: Tensor, b: Tensor, groups: int = 1) -> Tensor:
@@ -45,7 +51,8 @@ def to_ntuple(val: Union[int, Iterable[int]], n: int) -> Tuple[int, ...]:
         if len(out) == n:
             return out
         else:
-            raise ValueError(f"Cannot cast tuple of length {len(out)} to length {n}.")
+            raise ValueError(
+                f"Cannot cast tuple of length {len(out)} to length {n}.")
     else:
         return n * (val,)
 
@@ -78,7 +85,7 @@ def fft_conv(
     stride_ = to_ntuple(stride, n=signal.ndim - 2)
 
     # Pad the input signal & kernel tensors
-    signal_padding = [p for p in padding_[::-1] for _ in range(2)]
+    signal_padding = _reverse_repeat_tuple(padding_, 2)
     signal = f.pad(signal, signal_padding)
 
     # Because PyTorch computes a *one-sided* FFT, we need the final dimension to
@@ -87,6 +94,13 @@ def fft_conv(
         signal_ = f.pad(signal, [0, 1])
     else:
         signal_ = signal
+
+    lcm_padding = []
+    for i in range(signal.ndim-1, 1, -1):
+        lcm_padding += [0, lcm(signal_.size(i),
+                               stride_[i-2]) - signal_.size(i)]
+    # print(lcm_padding)
+    signal_ = f.pad(signal_, lcm_padding)
 
     kernel_padding = [
         pad
@@ -100,18 +114,66 @@ def fft_conv(
     signal_fr = rfftn(signal_, dim=tuple(range(2, signal.ndim)))
     kernel_fr = rfftn(padded_kernel, dim=tuple(range(2, signal.ndim)))
 
-    kernel_fr.imag *= -1
-    output_fr = complex_matmul(signal_fr, kernel_fr, groups=groups)
-    output = irfftn(output_fr, dim=tuple(range(2, signal.ndim)))
+    output_fr = complex_matmul(signal_fr, kernel_fr.conj(), groups=groups)
+
+    # symmetric
+    if signal.ndim > 3:
+        output_fr = fftshift(output_fr, dim=tuple(range(2, signal.ndim - 1)))
+        for i in range(2, signal.ndim - 1):
+            if stride_[i - 2] == 1:
+                continue
+            step = output_fr.size(i) // stride_[i - 2]
+            origin_center = output_fr.size(i) // 2
+            down_center = step // 2
+            window_start = origin_center - down_center
+
+            pad_front = window_start % step
+            if pad_front:
+                pad_front = step - pad_front
+
+            pad_back = (output_fr.size(i) - window_start) % step
+            if pad_back:
+                pad_back = step - pad_back
+
+            output_fr = f.pad(output_fr, [0, 0] * (signal.ndim - i - 1) + [pad_front, pad_back])
+            output_fr = output_fr.view(
+                *output_fr.shape[:i], -1, step, *output_fr.shape[i+1:]).sum(i) / stride_[i-2]
+
+        output_fr = ifftshift(output_fr, dim=tuple(range(2, signal.ndim - 1)))
+        output_fr = ifftn(output_fr, dim=tuple(range(2, signal.ndim - 1)))
+
+    #output = irfftn(output_fr, dim=-1)
+    output_fr = torch.cat(
+        (output_fr, output_fr.flip(-1)[..., 1:-1].conj()), dim=-1)
+    output_fr = fftshift(output_fr, dim=-1)
+    if stride_[-1] != 1:
+        step = output_fr.size(-1) // stride_[-1]
+        origin_center = output_fr.size(-1) // 2
+        down_center = step // 2
+        window_start = origin_center - down_center
+
+        pad_front = window_start % step
+        if pad_front:
+            pad_front = step - pad_front
+
+        pad_back = (output_fr.size(-1) - window_start) % step
+        if pad_back:
+            pad_back = step - pad_back
+
+        output_fr = f.pad(output_fr, [pad_front, pad_back])
+        output_fr = output_fr.view(
+            *output_fr.shape[:-1], -1, step).sum(-2) / stride_[-1]
+
+    output_fr = ifftshift(output_fr, dim=-1)
+    output = ifft(output_fr, dim=-1).real
 
     # Remove extra padded values
-    new_stride = [output.stride(0), output.stride(1)]
     new_size = [output.shape[0], output.shape[1]]
     for i in range(2, signal.ndim):
-        new_stride.append(output.stride(i) * stride_[i-2])
-        new_size.append((signal.size(i) - kernel.size(i)) // stride_[i - 2] + 1)
+        new_size.append((signal.size(i) - kernel.size(i)) //
+                        stride_[i - 2] + 1)
 
-    output = output.as_strided(new_size, new_stride).contiguous()
+    output = output.as_strided(new_size, output.stride()).contiguous()
 
     # Optionally, add a bias term before returning.
     if bias is not None:
